be8b4106cbf0c6b4e056ff183fa705b88b40286a
YoussefMahmoudOksha <http://youssef.mahmoud75359@gmail.com/>
2023-11-03 19:39:10 +0200

    add history command

--- Terminal.java
+++ Terminal.java
@@ -1,16 +1,14 @@
 import java.io.File;
 import java.nio.file.*;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 import java.io.IOException;
-import java.util.Objects;
 import java.nio.file.Files;
-import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.io.BufferedReader;
 import java.io.FileReader;
+
 
 public class Terminal {
     static public Parser parser = new Parser();
@@ -18,6 +16,8 @@ public class Terminal {
     static public Boolean play = true;
 
     private static Path currentDirectory = Paths.get("").toAbsolutePath();
+
+    private final List<String> commandHistory = new ArrayList<>();
 
 
     public void echo(){
@@ -253,26 +253,43 @@ public class Terminal {
         }
     }
 
+    public void addToHistory(String command) {
+        commandHistory.add(command);
+    }
+
+    public void history() {
+        System.out.println("Command History:");
+        for (int i = 0; i < commandHistory.size(); i++) {
+            System.out.println((i + 1) + ". " + commandHistory.get(i));
+        }
+    }
+
 
     public void chooseCommandAction() {
         String command = parser.getCommandName();
         String[] args = parser.getArgs();
+        
 
         if (Objects.equals(command, "echo")) {
             echo();
+            addToHistory("echo");
         } else if (Objects.equals(command, "exit")) {
             play = false;
         } else if (Objects.equals(command, "pwd")) {
             System.out.println(pwd());
+            addToHistory("pwd");
         } else if (Objects.equals(command, "ls")) {
             if (args.length == 1 && Objects.equals(args[0], "-r")) {
                 lsR();
+                addToHistory("ls -r");
             } else {
                 ls();
+                addToHistory("ls");
             }
         }else if (Objects.equals(command, "touch")) {
             if (args.length == 1 && args[0] != null) {
                 touch(args[0]);
+                addToHistory("touch");
             } else {
                 System.out.println("Invalid 'touch' command. Usage: touch <filename>");
             }
@@ -283,12 +300,14 @@ public class Terminal {
                     String destinationPath = args[2];
 
                     cpR(sourcePath, destinationPath);
+                    addToHistory("cp -r");
                 } else {
                     System.out.println("Invalid 'cp -r' command. Usage: cp -r <sourceDirectory> <destinationDirectory>");
                 }
             } else {
                 if (args.length == 2) {
                     cp(args[0], args[1]);
+                    addToHistory("cp");
                 } else {
                     System.out.println("Invalid 'cp' command. Usage: cp <sourceFile> <destinationFile>");
                 }
@@ -297,30 +316,39 @@ public class Terminal {
 
             if (args.length == 0){
                 cd();
+                addToHistory("cd");
             } else if (args.length == 1 && args[0] != null) {
                 cd(args[0]);
+                addToHistory("cd");
             }
 
         }else if (Objects.equals(command, "rm")) {
             if (args.length == 1 && args[0] != null) {
                 rm(args[0]);
+                addToHistory("rm");
             }else if(args.length == 0) {
                 System.out.println("Invalid 'rm' command. Usage: rm <file name> ");
             }
         }else if (Objects.equals(command, "rmdir")) {
             if (args.length == 1 && args[0] != null) {
                 rmdir(args[0]);
+                addToHistory("rmdir");
             }else if(args.length == 0) {
                 System.out.println("Invalid 'rmdir' command. Usage: rmdir <*> or <path directory> ");
             }
         }else if (Objects.equals(command, "wc")) {
             if (args.length == 1 && args[0] != null) {
                 wc(args[0]);
+                addToHistory("wc");
             }else if(args.length == 0) {
                 System.out.println("Invalid 'wc' command. Usage: wc <file name>");
             }
         }else if (Objects.equals(command, "cat")) {
             cat(args);
+            addToHistory("cat");
+        }else if (Objects.equals(command, "history")) {
+            history();
+            addToHistory("history");
         }else {
             System.out.println("Command not found");
         }
